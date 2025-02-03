f3567fbd17fe3735c4652c4c6442bd27c2248284
Henrique <henrique_krf@outlook.com>
2025-02-03 16:37:32 -0300

    operadores aritméticos

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int idade = 25;
+        double salarioMensal = 4500.00;
+        char genero = 'M';
+        boolean empregadoJava = true;
+
+        System.out.println("Idade: " + idade);
+        System.out.println("Salário: " + salarioMensal);
+        System.out.println("Gênero: " + genero);
+        System.out.println("Empregado: " + empregadoJava);
     }
 }
