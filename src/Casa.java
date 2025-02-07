fd7c8b58b66f006f407db88539b732ea0e80a837
Henrique <henrique_krf@outlook.com>
2025-02-07 17:29:16 -0300

    Métodos - POO

--- src/Casa.java
+++ src/Casa.java
@@ -1,18 +1,25 @@
+
 public class Casa {
-    public static void main(String[] args) {
-        //Criando o objeto casa
-        PlantaCasa casa = new PlantaCasa();
-
-        //Inicializando os atributos (variáveis) (características) casa
-        casa.numeroBanheiros = 2;
-        casa.numeroQuartos = 3;
-        casa.metragem = 70;
-        casa.material = "Tijolo";
-        casa.cor = "Cinza";
-
-        //Chamada da função (método) da ordem;
-        casa.contruir();
-
-        casa.pintar();
-    }
+//    public static void main(String[] args) {
+//        //Criando o objeto casa
+//        PlantaCasa casa = new PlantaCasa();
+//
+//        //Inicializando os atributos (variáveis) (características) casa
+//        casa.numeroBanheiros = 2;
+//        casa.numeroQuartos = 3;
+//        casa.metragem = 70;
+//        casa.material = "Tijolo";
+//        casa.cor = "Cinza";
+//
+//        //Chamada da função (método) da ordem;
+//        casa.contruir();
+//
+//        casa.pintar();
+//
+//        casa.mudarCorParede("Branco");
+//
+//        int resultado = casa.somarMetragem();
+//
+//        System.out.println(resultado);
+//    }
 }
